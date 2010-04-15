c008b50fab6da73412f735174631ab843da2816c
Johannes Brodwall <johannes@brodwall.com>
2010-04-15 23:28:31 +0200

    A web server for testing - exercise complete

---
+++ src/test/java/com/brodwall/kata/javaee/WebServer.java
@@ -0,0 +1,22 @@
+package com.brodwall.kata.javaee;
+
+import org.hsqldb.jdbc.jdbcDataSource;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.plus.naming.EnvEntry;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+public class WebServer {
+
+	public static void main(String[] args) throws Exception {
+		jdbcDataSource dataSource = new jdbcDataSource();
+		dataSource.setDatabase("jdbc:hsqldb:file:target/personDatabase");
+		dataSource.setUser("sa");
+		new EnvEntry("jdbc/personDs", dataSource);
+
+		Server server = new Server(8088);
+		server.addHandler(new WebAppContext("src/main/webapp", "/"));
+		server.start();
+
+		System.out.println("http://localhost:8088/");
+	}
+}
